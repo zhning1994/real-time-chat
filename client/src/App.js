5c09a74340ac5f2c9f64d7191ff79b2617ec77c0
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-07 23:49:56 +0800

    setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
+import Chat from './pages/Chat.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Hihi
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
