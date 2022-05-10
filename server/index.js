366a88bcfd50bafc2b27b3076c70c2d2d0c0968d
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-10 19:27:50 +0800

    add /api/auth for everything coming from  userRouters

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use("/api/auth", userRoutes);
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((err) => console.log(err.message));
