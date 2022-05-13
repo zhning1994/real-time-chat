9a666df93087629a457acc6acbbd48bd4f8c7b41
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-13 18:35:09 +0800

    add setAvatar route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Register from './pages/Register.jsx';
 import Login from './pages/Login.jsx';
 import Chat from './pages/Chat.jsx';
+import SetAvatar from './pages/SetAvatar.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path='/' element={<Chat />} />
       </Routes>
     </BrowserRouter>
