41ca923227526328404c25d27e744d922b8d931f
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-16 18:36:21 +0800

    add logic to logout buttom, once click then clear local storage and navigate to login page

---
+++ client/src/components/Logout.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+import { BiPowerOff } from "react-icons/bi";
+
+function Logout() {
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+  return (
+    <Button onClick={handleClick}>
+      <BiPowerOff />
+    </Button>
+  );
+}
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  border: none;
+  background-color: #db3a34;
+  cursor: pointer;
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
+`;
+
+export default Logout;
