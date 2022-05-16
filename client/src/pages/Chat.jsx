950f39764f2504651c64fddd4c78293a42726a92
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-16 13:34:05 +0800

    add in Welcome component for selected new user to chat, pass down contacts. current user and change chat handler to contact component

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,15 +3,19 @@ import styled from "styled-components";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { allUsersRoute } from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+
 function Chat() {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
 
   useEffect(() => {
     async function userCheck() {
-      if (localStorage.getItem("chat-app-user")) {
-        navigate("/");
+      if (!localStorage.getItem("chat-app-user")) {
+        navigate("/login");
       } else {
         setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
       }
@@ -33,12 +37,25 @@ function Chat() {
     }
 
     user();
-  }, [currentUser]);
+  }, [navigate, currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
 
   return (
-    <Container>
-      <div className="container"></div>
-    </Container>
+    <>
+      <Container>
+        <div className="container">
+          <Contacts
+            contacts={contacts}
+            currentUser={currentUser}
+            changeChat={handleChatChange}
+          />
+          <Welcome currentUser={currentUser} />
+        </div>
+      </Container>
+    </>
   );
 }
 
@@ -48,13 +65,13 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
   gap: 1rem;
-  background-color: #084c61;
+  align-items: center;
+  background-color: #323031;
   .container {
     height: 85vh;
     width: 85vw;
-    background-color: #323031;
+    background-color: #084c61;
     display: grid;
     grid-template-columns: 25% 75%;
     @media screen and (min-width: 720px) and (max-width: 1080px) {
