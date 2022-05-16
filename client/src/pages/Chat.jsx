739bdb058544c085bef9a4eab90411ab623bb66e
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-16 13:05:15 +0800

    check if current user already login, if have current user info at local storage, get all users other than current user, pass all other users to contact props

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,7 +1,66 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../utils/APIRoutes";
 function Chat() {
-  return <div>Chat</div>;
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    async function userCheck() {
+      if (localStorage.getItem("chat-app-user")) {
+        navigate("/");
+      } else {
+        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+      }
+    }
+
+    userCheck();
+  }, [navigate]);
+
+  useEffect(() => {
+    async function user() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    }
+
+    user();
+  }, [currentUser]);
+
+  return (
+    <Container>
+      <div className="container"></div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background-color: #084c61;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #323031;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
 export default Chat;
