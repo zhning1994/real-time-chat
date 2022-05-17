0f4f7504decac23f366a04a447a42166f9378306
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-17 19:36:30 +0800

    reinport controller as variable name changing from getAllMessage to getMessages

--- server/routes/messagesRoute.js
+++ server/routes/messagesRoute.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { addMessage, getAllMessage } from "../controllers/messagesController.js";
+import { addMessage, getMessages } from "../controllers/messagesController.js";
 
 
 const router = express.Router();
 
 router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getAllMessage);
-
+router.post("/getmsg/", getMessages);
 
 export default router;
