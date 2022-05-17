22a6ed85edbbc915ea33cd50a09bbd56bbe21fc0
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-17 19:42:35 +0800

    in getMessages controller, find all the messages which fulfill the users requirement and return projectedMessages which fromSelf is boolean and message

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,23 +1,36 @@
-import messageModel from "../model/messageModel.js";
+import Messages from "../model/messageModel.js";
 
 export const addMessage = async (req, res, next) => {
     try {
         const { from, to, message } = req.body;
-        const data = await messageModel.create({
+        const data = await Messages.create({
             message: { text: message },
             users: [from, to],
             sender: from,
         });
         if (data) return res.json({ msg: "Message added successfully." });
-        return res.json({ msg: "Failed to add message to the database." });
+        else return res.json({ msg: "Failed to add message to the database." });
     } catch (error) {
         next(error);
     }
 };
 
-export const getAllMessage = async (req, res, next) => {
+export const getMessages = async (req, res, next) => {
     try {
+        const { from, to } = req.body;
+        const messages = await Messages.find({
+            users: {
+                $all: [from, to]
+            },
+        }).sort({ updatedAt: 1 });
 
+        const projectedMessages = messages.map((msg) => {
+            return {
+                fromSelf: msg.sender.toString() === from,
+                message: msg.message.text,
+            };
+        });
+        res.json(projectedMessages);
     } catch (error) {
         next(error);
     }
