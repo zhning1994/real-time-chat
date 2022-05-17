0e8c91b73eac9ade2567a37ab7b19db8c159f445
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-17 19:43:36 +0800

    add receive message route

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,6 +1,8 @@
 const host = "http://localhost:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
+export const logoutRoute = `${host}/api/auth/logout`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const receiveMessageRoute = `${host}/api/messages/getmsg`;
