8c1909a06a146e18fcf04e32d754838a8500b74d
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-17 17:16:17 +0800

    applied messagesRoute with the api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from "./routes/userRoutes.js";
+import messagesRoute from "./routes/messagesRoute.js";
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", userRoutes);
+app.use("/api/messages", messagesRoute);
+
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((err) => console.log(err.message));
