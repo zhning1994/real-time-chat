8c79feccce51b1593fb38a6e2dc942db20cba740
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-18 23:09:45 +0800

    delete user id in  onlineUsers so that can logout the app

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -69,3 +69,13 @@ export const getAllUsers = async (req, res, next) => {
         next(error);
     }
 }
+
+export const logOut = (req, res, next) => {
+    try {
+        if (!req.params.id) return res.json({ msg: "User id is required " });
+        onlineUsers.delete(req.params.id);
+        return res.status(200).send();
+    } catch (error) {
+        next(error);
+    }
+};
