3755cfec367036b43a606fedd8e9c4e2035a6865
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-18 22:50:12 +0800

    export host variable as socket client needed

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:5000";
+export const host = "http://localhost:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const logoutRoute = `${host}/api/auth/logout`;
