4def835b05486ea756cf55a8ed293bd49c8d71ad
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-18 23:11:44 +0800

    reamend server connection code and setup io and socket for add-user and send-msg

--- server/index.js
+++ server/index.js
@@ -4,15 +4,47 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from "./routes/userRoutes.js";
 import messagesRoutes from "./routes/messagesRoute.js";
+import { Server } from 'socket.io';
 
 const app = express();
 dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth", userRoutes);
-app.use("/api/messages", messagesRoutes);
 
 const PORT = process.env.PORT;
 
-mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((err) => console.log(err.message));
+mongoose.connect(process.env.CONNECTION_URL).then(() => {
+    console.log("DB Connetion Successfull");
+}).catch((err) => {
+    console.log(err.message);
+});
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messagesRoutes);
+
+const server = app.listen(PORT, () =>
+    console.log(`Server started on ${process.env.PORT}`)
+);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+        }
+    });
+});
