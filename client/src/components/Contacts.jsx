f5b36c00f3319483a276692541a8b708cbe92be7
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-19 14:35:18 +0800

    delete unused props input

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
 
-function Contacts({ contacts, currentUser, changeChat }) {
+function Contacts({ contacts, changeChat }) {
   const [currentUserName, setCurrentUserName] = useState(undefined);
   const [currentUserImage, setCurrentUserImage] = useState(undefined);
   const [currentSelected, setCurrentSelected] = useState(undefined);
